8a68c3790c8f0754c9f47f0d612f0cb76d758069
lopikss <100728779+lopikss@users.noreply.github.com>
2022-03-21 17:31:06 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("Crouching---------------------------------------------------------------------------")]
     [SerializeField] private bool canPlayerCrouch;
-    [SerializeField] [Range(0, 15f)] private float crouchSpeed = 1f;
+    [SerializeField] [Range(0, 1f)] private float crouchSpeed = 1f;
     [SerializeField] private Collider2D CrouchDisableCollider1;
     [SerializeField] private Collider2D CrouchDisableCollider2;
 
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] [Range(0f, 10f)] private float LowJumpMultiplier = 2f;
 
     [Header("Ground checker ---------------------------------------------------------------------")]
-    [SerializeField] private Transform GroundCheckObject = null;
+    [SerializeField] private Transform GroundCheck = null;
     [SerializeField] private bool boxCheck;
     [SerializeField] private bool cirleCheck;
     [SerializeField] [Range(0, 3f)] private float boxHeight = 1f;
@@ -31,6 +31,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] [Range(0, 3f)] private float circleSize = 1f;
     [SerializeField] private LayerMask WhatIsGround;
     [SerializeField] private bool _isGrounded = false;
+
+    [Header("Cieling checker---------------------------------------------------------------------")]
+    [SerializeField] private Transform CielingCheck = null;
+    [SerializeField] [Range(0, 3f)] private float boxHeights = 1f;
+    [SerializeField] [Range(0, 3f)] private float boxWidths = 1f;
+    [SerializeField] private bool boxChecks;
 
 
     private float _movementDirection;
@@ -40,6 +46,7 @@ public class PlayerController : MonoBehaviour
     private bool isJumping = false;
     private bool isDoubleJumping;
     public bool isCrouching;
+    private bool isCrouching2;
 
     readonly private Collider2D[] colliders = new Collider2D[4];
 
@@ -50,7 +57,7 @@ public class PlayerController : MonoBehaviour
     private int so_doubleJump;
 
 
-   
+
 
 
     void Start()
@@ -79,17 +86,18 @@ public class PlayerController : MonoBehaviour
         // makes player crouch
         if (canPlayerCrouch)
         {
-            if (Input.GetButtonDown("Crouch") && _isGrounded)
+            if (Input.GetButtonDown("Crouch"))
             {
-                MovementSpeed -= crouchSpeed;
                 isCrouching = true;
+                MovementSpeed *= crouchSpeed;
             }
             if (Input.GetButtonUp("Crouch"))
             {
-                MovementSpeed += crouchSpeed;
                 isCrouching = false;
+                MovementSpeed /= crouchSpeed;
             }
         }
+
 
         // makes player hitbox smaller when crouching
         if (isCrouching == true)
@@ -140,7 +148,7 @@ public class PlayerController : MonoBehaviour
             isJumping = false;
         }
 
-        // makes player double jump jump
+        // makes player double jump
         if (isDoubleJumping)
         {
             _rigidbody2D.AddForce(Vector2.up * doubleJumpPower, ForceMode2D.Impulse);
@@ -161,13 +169,12 @@ public class PlayerController : MonoBehaviour
             _rigidbody2D.gravityScale = 1f;
         }
 
-
         //Box collider
         if (boxCheck == true)
         {
             ContactFilter2D filter2D = new ContactFilter2D();
             filter2D.SetLayerMask(WhatIsGround);
-            var numberOfColliders = Physics2D.OverlapBox(GroundCheckObject.position, new Vector2(boxWidth, boxHeight), 0f, filter2D, colliders);
+            var numberOfColliders = Physics2D.OverlapBox(GroundCheck.position, new Vector2(boxWidth, boxHeight), 0f, filter2D, colliders);
             if (numberOfColliders > 0) _isGrounded = true;
         }
 
@@ -176,7 +183,7 @@ public class PlayerController : MonoBehaviour
         {
             ContactFilter2D filter2D = new ContactFilter2D();
             filter2D.SetLayerMask(WhatIsGround);
-            var numberOfColliders = Physics2D.OverlapCircle(GroundCheckObject.position, circleSize, filter2D, colliders);
+            var numberOfColliders = Physics2D.OverlapCircle(GroundCheck.position, circleSize, filter2D, colliders);
             if (numberOfColliders > 0) _isGrounded = true;
         }
     }
@@ -189,16 +196,23 @@ public class PlayerController : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (GroundCheckObject != null && boxCheck == true)
+        if (GroundCheck != null && boxCheck == true)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(GroundCheckObject.position, new Vector3(boxWidth, boxHeight, 0f));
+            Gizmos.DrawWireCube(GroundCheck.position, new Vector3(boxWidth, boxHeight, 0f));
 
         }
-        if (GroundCheckObject != null && cirleCheck == true)
+        if (GroundCheck != null && cirleCheck == true)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(GroundCheckObject.position, circleSize);
+            Gizmos.DrawWireSphere(GroundCheck.position, circleSize);
+
+        }
+
+        if (CielingCheck != null && boxCheck == true)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(CielingCheck.position, new Vector3(boxWidths, boxHeights, 0f));
 
         }
     }
